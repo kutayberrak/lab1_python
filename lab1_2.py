d32328e997ed9868b9c686c4038efa9cc24df1e3
kutayberrak <kutayskb@gmail.com>
2023-03-07 13:06:11 +0300

    Initial commit

---
+++ lab1_2.py
@@ -0,0 +1,9 @@
+var1 = int(input("Enter first variable "))
+var2 = int(input("Enter second variable "))
+sum = var1 + var2
+diff = (var1 - var2)
+prod = var1 * var2
+print("Variables are:",var1,",",var2)
+print("Summation:",sum)
+print("Difference:",diff)
+print("Multiplication:",prod)
---
+++ myFirstLabScript.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+print("hello",name)
+id = input("What is your student id? ")
+print("Your id is " , id)
